27778b7a72ebacfd38b5d4dfc89f03e539d27149
Herfando <herfando_91@yahoo.com>
2025-08-28 20:01:56 +0700

    edit infer

--- infer.ts
+++ infer.ts
@@ -1,4 +1,8 @@
-type GetArrayItem<T> = T extends (infer U)[] ? U : T;
+type GetReturnItem<T> = T extends (infer U)[] ? U : T;
 
-type A = GetArrayItem<string[]>; // string
-type B = GetArrayItem<number[]>; // number
+type A = GetReturnItem<string[]>; // string
+type B = GetReturnItem<number>; // number
+type C = GetReturnItem<Custom>;
+
+console.log(A);
+console.log(B);
